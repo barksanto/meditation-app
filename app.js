74bbe7656f2755c8e3f719ac65756c7acc5377e7
Barkley Santo <barkley.santo@gmail.com>
2020-12-20 15:06:34 -0600

    query selectors for buttons

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = () => {
+  const song = document.querySelector('.song');
+  const play = document.querySelector('.play');
+  const outline = document.querySelector('.moving-outline circle');
+  cont video = document.querySelector('.vid-container video');
+
+}
+
+app();
