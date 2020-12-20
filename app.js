20ec466ffc401edde38548351f5934353a50d61f
Barkley Santo <barkley.santo@gmail.com>
2020-12-20 15:16:27 -0600

    get total length of outline

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const app = () => {
   const song = document.querySelector('.song');
   const play = document.querySelector('.play');
   const outline = document.querySelector('.moving-outline circle');
-  cont video = document.querySelector('.vid-container video');
+  const video = document.querySelector('.vid-container video');
 
-}
+
+  //sounds
+  const sounds = document.querySelectorAll('.sound-picker button');
+  //time display
+  const timeDisplay = document.querySelector('.timeDisplay');
+  //get length of outline
+  const outlineLength = outline.getTotalLength();
+  // console.log(outlineLength)
+  // 'fake' Duration - 
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+//play sound
+  play.addEventListener('click', () => {
+    song.play();
+  });
+// create a function to stop and play the sounds
+};
 
 app();
